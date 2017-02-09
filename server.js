a7867eee6ca4736de9acceeb8f3fa63d52350b6f
Surasak La-ongkham <surasak.ict.msu@gmail.com>
2017-02-09 09:08:15 +0700

    Show timestamp on UI

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var PORT = process.env.PORT || 3000;
+var moment = require('moment');
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
@@ -13,13 +14,19 @@ io.on('connection', function(socket) {
 	socket.on('message', function(message) {
 		console.log('Message received: ' + message.text);
 
+		message.timestamp = moment().valueOf();
+
 		//socket.broadcast.emit('message', message);
 		io.emit('message', message);
 	});
 
+	// timestamp property - Javascript timestamp (milliseconds)
+
+
 	// send to client
 	socket.emit('message', {
-		text: 'Welcome to the chat application!'
+		text: 'Welcome to the chat application!',
+		timestamp: moment().valueOf()
 	});
 });
 
