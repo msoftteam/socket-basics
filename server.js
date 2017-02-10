e4d332f9139aae2360e216201d931de93769a165
Surasak La-ongkham <surasak.ict.msu@gmail.com>
2017-02-10 10:10:36 +0700

    Add @currentUsers command

--- server.js
+++ server.js
@@ -9,6 +9,30 @@ app.use(express.static(__dirname + '/public'));
 
 var clientInfo = {};
 
+// send current user to provided socket
+function sendCurrentUsers(socket) {
+	var info = clientInfo[socket.id];
+	var users = [];
+
+	if (typeof info === 'undefined') {
+		return;
+	}
+
+	Object.keys(clientInfo).forEach(function(socketId) {
+		var userInfo = clientInfo[socketId];
+
+		if (info.room === userInfo.room) {
+			users.push(userInfo.name);
+		}
+	});
+
+	socket.emit('message', {
+		name: 'System',
+		text: 'Current users: ' + users.join(', '),
+		timestamp: moment().valueOf()
+	});
+}
+
 io.on('connection', function(socket) {
 	console.log('User connected via socket.io!');
 
@@ -40,10 +64,14 @@ io.on('connection', function(socket) {
 	socket.on('message', function(message) {
 		console.log('Message received: ' + message.text);
 
-		message.timestamp = moment().valueOf();
+		if (message.text === '@currentUsers') {
+			sendCurrentUsers(socket);
+		} else {
+			message.timestamp = moment().valueOf();
 
-		//socket.broadcast.emit('message', message);
-		io.to(clientInfo[socket.id].room).emit('message', message);
+			//socket.broadcast.emit('message', message);
+			io.to(clientInfo[socket.id].room).emit('message', message);
+		}
 	});
 
 	// timestamp property - Javascript timestamp (milliseconds)
